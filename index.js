7da3ab45642876358db109bf6bdf1cd0cf6a6d40
Adamkoda2306 <147936412+Adamkoda2306@users.noreply.github.com>
2025-03-20 16:49:34 +0530

    mysql updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
@@ -17,18 +17,24 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// MySQL Connection
+require('dotenv').config();
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'adam123',
-    database: 'database_website'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL Connected...');
+    if (err) {
+        console.error('MySQL connection error:', err);
+        return;
+    }
+    console.log('Connected to MySQL on Railway');
 });
+
 
 // Configure multer for file uploads
 const storage = multer.memoryStorage();
